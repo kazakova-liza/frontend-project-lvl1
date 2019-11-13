8bdc408ae7fccd33319a6b967d45dbc3713fc7c3
Liza Kazakova <kazakova.uk@gmail.com>
2019-11-13 11:17:47 +0000

    added brain-even.js, edited package.json, edited index.js`

--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import readlineSync from 'readline-sync';
 export const sayHello = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-};
-
-export const sayHelloWorld = () => {
-  console.log('Hello world!');
-};
+}
