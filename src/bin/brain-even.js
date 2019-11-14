205d7f52d69d0dba72ad5685d1bb99717ca0d506
Liza Kazakova <kazakova.uk@gmail.com>
2019-11-14 14:52:55 +0000

    Added showQuestion and showAnswer functions to brain-even.js

---
+++ src/bin/brain-even.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import { sayHello } from '..';
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if the number is even, otherwise answer "no"');
+
+sayHello();
+
--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ export const sayHello = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
+
+export const showQuestion = () => {
+  const question = `Question:${Math.floor(Math.random() * 10)}`;
+  console.log(question);
+};
+
+export const showAnswer = () => {
+  const answer = readlineSync.question('Your answer: ');
+  console.log(answer);
+};
